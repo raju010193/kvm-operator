0d40f2cef90c7a388095ad1acf3703a73259190a
sidhu140 <swamy.sidhu1@gmail.com>
2020-07-21 21:17:24 +0530

    controller not used import deleted

--- pkg/controller/kvm/kvm_controller.go
+++ pkg/controller/kvm/kvm_controller.go
@@ -7,7 +7,7 @@ import (
 	kvmv1alpha1 "github.com/raju140/kvm-operator/pkg/apis/kvm/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	//"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
